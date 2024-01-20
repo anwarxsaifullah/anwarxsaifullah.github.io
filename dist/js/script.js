4ec8e69cfaf588f03c2599f37418a3cf16f206d5
Anwar S <anwarxsaifullah+git@gmail.com>
2024-01-20 20:11:16 +0000

    Fix event on hamburger-line

--- dist/js/script.js
+++ dist/js/script.js
@@ -21,13 +21,14 @@ window.onscroll = function(){
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
-hamburger.addEventListener('click', function () {
+hamburger.parentElement.addEventListener('click', function () {
   hamburger.classList.toggle('hamburger-active');
   navMenu.classList.toggle('hidden');
 });
 
 
 window.addEventListener('click', function(e){
+  console.log(e.target)
   if(e.target != hamburger && e.target != navMenu){
     hamburger.classList.remove('hamburger-active');
     navMenu.classList.add('hidden');
